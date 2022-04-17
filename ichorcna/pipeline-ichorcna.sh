b75ff8686b868fc6cfa48b1b32077dbe13982434
homijan <homijan@gmail.com>
2022-04-17 09:20:55 -0700

    Bugfix using wigfile.
    conda activate working in bash script.

--- ichorcna/pipeline-ichorcna.sh
+++ ichorcna/pipeline-ichorcna.sh
@@ -11,21 +11,21 @@ ICHORDIR="/drive3/aliciapm/ichorCNA"
 mkdir ${WRKDIR}/files-tumor
 mkdir ${WRKDIR}/files-normal
 ./generate-offtarget-index.sh
-# Activate the hmmcopy environment (give it 1 minute to be activated)
-conda activate hmmcopy && sleep 1m
+# Activate the hmmcopy environment (source explicit conda used by apm)
+source /drive3/aliciapm/anaconda3/etc/profile.d/conda.sh && conda activate hmmcopy
 ./generate-wig.sh
+# Activate the cnvkit environment (source explicit conda used by apm)
+source /drive3/aliciapm/anaconda3/etc/profile.d/conda.sh && conda activate cnvkit
 ./generate-normal-reference.sh
 # Loop over offtarget bam files of the tumor samples.
 mkdir ${WRKDIR}/results-ichorcna
-# Activate the cnvkit environment (give it 1 minute to be activated)
-conda activate cnvkit && sleep 1m
 for wigfile in ${WRKDIR}/files-tumor/*.offtarget.bam.wig
 do
   # Weird trimming using sed to extract the hemestamp label
   TESTID="$(echo ${wigfile} | sed 's/.*Sample_//g' | sed 's/-T1_Tumor.*//g')"
   if [ ! -d ${WRKDIR}/results-ichorcna/${TESTID} ]; then
     # Run ichorcna to get final CNA results.
-    Rscript ${ICHORDIR}/scripts/runIchorCNA.R --id $TESTID --WIG $i --normal "c(0.5,0.6,0.7,0.8,0.9)" --maxCN 5 --gcWig   ${ICHORDIR}/inst/extdata/gc_hg19_1000kb.wig --mapWig ${ICHORDIR}/inst/extdata/map_hg19_1000kb.wig --centromere ${ICHORDIR}/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt --normalPanel ${WRKDIR}/reference-normal_median.rds --includeHOMD False --chrs "c(1:22, \"X\")" --chrTrain "c(1:22)" --estimateNormal true --estimatePloidy True --scStates "c(1,3)" --txnStrength 1000 --outDir ${WRKDIR}/results-ichorcna
+    Rscript ${ICHORDIR}/scripts/runIchorCNA.R --id $TESTID --WIG $wigfile --normal "c(0.5,0.6,0.7,0.8,0.9)" --maxCN 5 --gcWig   ${ICHORDIR}/inst/extdata/gc_hg19_1000kb.wig --mapWig ${ICHORDIR}/inst/extdata/map_hg19_1000kb.wig --centromere ${ICHORDIR}/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt --normalPanel ${WRKDIR}/reference-normal_median.rds --includeHOMD False --chrs "c(1:22, \"X\")" --chrTrain "c(1:22)" --estimateNormal true --estimatePloidy True --scStates "c(1,3)" --txnStrength 1000 --outDir ${WRKDIR}/results-ichorcna
   fi
 done
 
@@ -37,6 +37,6 @@ do
   TESTID="$(echo ${wigfile} | sed 's/.*Sample_//g' | sed 's/-T1_Tumor.*//g')"
   if [ ! -d ${WRKDIR}/results-ichorcna/${TESTID} ]; then
     # Run ichorcna to get final CNA results.
-    Rscript ${ICHORDIR}/scripts/runIchorCNA.R --id $TESTID --WIG $i --normal "c(0.5,0.6,0.7,0.8,0.9)" --maxCN 5 --gcWig   ${ICHORDIR}/inst/extdata/gc_hg19_1000kb.wig --mapWig ${ICHORDIR}/inst/extdata/map_hg19_1000kb.wig --centromere ${ICHORDIR}/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt --normalPanel ${WRKDIR}/reference-normal_median.rds --includeHOMD False --chrs "c(1:22, \"X\")" --chrTrain "c(1:22)" --estimateNormal true --estimatePloidy True --scStates "c(1,3)" --txnStrength 1000 --outDir ${WRKDIR}/results-ichorcna-normals
+    Rscript ${ICHORDIR}/scripts/runIchorCNA.R --id $TESTID --WIG $wigfile --normal "c(0.5,0.6,0.7,0.8,0.9)" --maxCN 5 --gcWig   ${ICHORDIR}/inst/extdata/gc_hg19_1000kb.wig --mapWig ${ICHORDIR}/inst/extdata/map_hg19_1000kb.wig --centromere ${ICHORDIR}/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt --normalPanel ${WRKDIR}/reference-normal_median.rds --includeHOMD False --chrs "c(1:22, \"X\")" --chrTrain "c(1:22)" --estimateNormal true --estimatePloidy True --scStates "c(1,3)" --txnStrength 1000 --outDir ${WRKDIR}/results-ichorcna-normals
   fi
 done
