3cf6ac5de478113ec4cc6651b1e803d8997aeaf0
homijan <homijan@gmail.com>
2022-04-17 09:35:48 -0700

    Check if the file reference-normal_median.rds exists.

--- ichorcna/generate-normal-reference.sh
+++ ichorcna/generate-normal-reference.sh
@@ -2,10 +2,12 @@
 # Run generate-wig-normal.sh to generate wig files from normal samples
 WRKDIR=$PWD
 ICHORDIR="/drive3/aliciapm/ichorCNA"
-# Create a text file listing wig files of normal samples
-ls ${WRKDIR}/files-normal/*.wig > normal-wigs.txt
-# Call R and run the script
-Rscript  ${ICHORDIR}/scripts/createPanelOfNormals.R --filelist normal-wigs.txt --gcWig ${ICHORDIR}/inst/extdata/gc_hg19_1000kb.wig --mapWig ${ICHORDIR}/inst/extdata/map_hg19_1000kb.wig --centromere ${ICHORDIR}/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt --outfile ${WRKDIR}/reference-normal
-
+if [ ! -f ${WRKDIR}/reference-normal_median.rds ]; then
+  echo "File ${WRKDIR}/reference-normal_median.rds does not exist."
+  # Create a text file listing wig files of normal samples
+  ls ${WRKDIR}/files-normal/*.wig > normal-wigs.txt
+  # Call R and run the script
+  Rscript  ${ICHORDIR}/scripts/createPanelOfNormals.R --filelist normal-wigs.txt --gcWig ${ICHORDIR}/inst/extdata/gc_hg19_1000kb.wig --mapWig ${ICHORDIR}/inst/extdata/map_hg19_1000kb.wig --centromere ${ICHORDIR}/inst/extdata/GRCh37.p13_centromere_UCSC-gapTable.txt --outfile ${WRKDIR}/reference-normal
+fi
 
 
