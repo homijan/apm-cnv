0f8ae8c523cee451dfa4900b9f350469a72d0ad2
homijan <homijan@gmail.com>
2022-04-27 11:01:00 -0700

    Proper definition of genplotsSE.

--- CNAdefs.py
+++ CNAdefs.py
@@ -12,6 +12,22 @@ CNA_original['trisomy12'] = ['chr12', '12p11.1', 33300001, 38200000]
 CNA_original['13q'] = ['chr13', '13q14.3', 50900001, 55300000]
 CNA_original['17p'] = ['chr17', '17p13.1', 6500001, 10700000]
 CNA_original['20q'] = ['chr20', '20q12', 37600001, 41700000]
+
+# Describe CNA_paper
+CNA_paper = {}
+CNA_paper['1p'] = ['chr1', '1p32', 50700001, 61300000]
+CNA_paper['1q'] = ['chr1', '1q21', 142600001, 155000000]
+CNA_paper['5q'] = ['chr5', '5q31', 131892957, 138269817]
+CNA_paper['mono5'] = ['chr5', '5p15.2', 1, 180915260]
+CNA_paper['7q'] = ['chr7', '7q31', 105507981, 116436182]
+CNA_paper['mono7'] = ['chr7', '7p11.1-q11.1', 1, 159138663]
+CNA_paper['trisomy8'] = ['chr8', '8p11.1-q11.1', 1, 146364022]
+CNA_paper['11q'] = ['chr11', '11q22.3', 102900001, 110400000]
+CNA_paper['trisomy12'] = ['chr12', '12p11.1', 33300001, 38200000]
+CNA_paper['13q'] = ['chr13', '13q14.3', 50900001, 55300000]
+CNA_paper['17p'] = ['chr17', '17p13.1', 6500001, 10700000]
+CNA_paper['20q'] = ['chr20', '20q12', 40709452, 41818422]
+
 # Indexes to CNA_original values
 iChr = 0; iCyt = 1; iStart = 2; iEnd = 3
 
@@ -24,4 +40,4 @@ ichorcnaRegionFormat = 'ichorcnaRegionFormat'
 ichorcnaSegmentFormat = 'ichorcnaSegmentFormat'
 
 # Define which CNA to use
-CNA = CNA_original
+CNA = CNA_paper
--- generateplotsSE.py
+++ generateplotsSE.py
@@ -1,16 +1,35 @@
-# Import Library
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#import pandas as pd 
-import matplotlib.pyplot as plt 
+cnvkit_segment_MDS = pd.read_table('cnvkit-segment-MDS-tableSE.txt', delimiter=" ")
+cnvkit_segment_CLL = pd.read_table('cnvkit-segment-CLL-tableSE.txt', delimiter=" ")
+cnvkit_segment_MM = pd.read_table('cnvkit-segment-MM-tableSE.txt', delimiter=" ")
+
+ichorcna_segment_MDS = pd.read_table('ichorcna-segment-MDS-tableSE.txt', delimiter=" ")
+ichorcna_segment_CLL = pd.read_table('ichorcna-segment-CLL-tableSE.txt', delimiter=" ")
+ichorcna_segment_MM = pd.read_table('ichorcna-segment-MM-tableSE.txt', delimiter=" ")
+
+labelsMDS = ['-5q', '-7q', '-20q', '+8']
+labelsCLL = ['-11q', '-13q', '-17p', '+12']
+labelsMM = ['-1p', '+1q', '-13q', '-17p']
+
+dataframes = {}
+dataframes['cnvkit-segment-MDS-tableSE'] = [cnvkit_segment_MDS, labelsMDS]
+dataframes['cnvkit-segment-CLL-tableSE'] = [cnvkit_segment_CLL, labelsCLL]
+dataframes['cnvkit-segment-MM-tableSE'] = [cnvkit_segment_MM, labelsMM]
+dataframes['ichorcna-segment-MDS-tableSE'] = [ichorcna_segment_MDS, labelsMDS]
+dataframes['ichorcna-segment-CLL-tableSE'] = [ichorcna_segment_CLL, labelsCLL]
+dataframes['ichorcna-segment-MM-tableSE'] = [ichorcna_segment_MM, labelsMM]
 
 
-# Plot multiple columns bar chart
-
-data = pd.read_table('cnvkit-segment-MDS-tableSE.txt', delimiter=" ")
-df = pd.DataFrame(data, columns=["Name","English","Hindi","Maths", "Science", "Computer"])
-
-df.plot(x="Lesion", y=["Sensitivity","Specificity"], kind="bar",figsize=(9,8))
-
-# Show
-
-plt.show()
+x = [0, 1, 2, 3]
+for key in dataframes:
+  df = dataframes[key][0]
+  labels = dataframes[key][1]  
+  df.plot(x="Lesion", y=["Sensitivity", "Specificity"], kind="bar",figsize=(7,7), colormap='tab10')
+  plt.xticks(x, labels, size = 20, rotation = 0 )
+  plt.legend(loc=(0.8,1))
+  plt.gca().spines['right'].set_color('none')
+  plt.gca().spines['top'].set_color('none')
+  plt.xlabel("")
+  plt.savefig(key+'.png')
