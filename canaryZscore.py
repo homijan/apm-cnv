33cb158d2afb96f78bac53b1c67e9a98dc0a4568
homijan <homijan@gmail.com>
2022-04-28 13:56:38 -0700

    Generalization of weighte-mean-value for canary.

--- canaryZscore.py
+++ canaryZscore.py
@@ -1,68 +1,67 @@
 from CNAdefs import *
 import pandas as pd
 
+# Obtain weighted-mean value based on overlap of clinical deletion/gain and CNV intervals in bed coordinates
 def canary_zscore(fileName):
+  chrColName = "#chr"; startColName = "start"; endColName = "end"; valueColName = "gc.corrected.norm.log.std.index.zWeighted.Final"
   # Read the data frame
   df = pd.read_csv(fileName,sep="\t")
   debug = False
-  w_cns = {}
+  w_values = {}
   for cna in cnas:
     if (debug):
       print(f'CNA bed interval [{CNA[cna][iStart]}, {CNA[cna][iEnd]}]')
-    with open(fileName) as f:
-      # Skip the first line
-      next(f)
-      cnTotal = 0
-      bedsizeTotal = 1e-32 # Numerical zero
-      # Loop over all lines
-      iLine = -1
-      for line in f:
-        iLine = iLine + 1
-        cols = line.split()
-        # canary file
-        chromosome = df["#chr"][iLine]; bedStart = df["start"][iLine]; bedEnd = df["end"][iLine]; cn = df["gc.corrected.norm.log.std.index.zWeighted.Final"][iLine]
-        #print(f'iLine {iLine}, cna {cna}, chromosome {chromosome}, start {bedStart}, end {bedEnd}, zScore {cn}, cnTotal {cnTotal}')  
-        if (not pd.isna(cn)):
-          if (CNA[cna][iChr] == chromosome):
-            # Segment smaller than CNA interval
-            if (bedStart > CNA[cna][iStart] and bedEnd < CNA[cna][iEnd]):
-              bedsize = bedEnd - bedStart
-              cnTotal = cnTotal + cn * bedsize
-              bedsizeTotal = bedsizeTotal + bedsize
-              if (debug):
-                print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
-                print(f'CNA {cna}, cn {cn}, bedsize {bedsize}')
-            # CNA interval smaller than segment
-            if (CNA[cna][iStart] >= bedStart and CNA[cna][iEnd] <= bedEnd):
-              bedsize = CNA[cna][iEnd] - CNA[cna][iStart]
-              cnTotal = cnTotal + cn * bedsize
-              bedsizeTotal = bedsizeTotal + bedsize
-              if (debug):
-                print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
-                print(f'CNA {cna}, cn {cn}, bedsize {bedsize}')
-            # Overlap of CNA interval and segment
-            else:
-              if (CNA[cna][iStart] >= bedStart and CNA[cna][iStart] <= bedEnd):
-                bedsize = bedEnd - CNA[cna][iStart]
-                cnTotal = cnTotal + cn * bedsize
-                bedsizeTotal = bedsizeTotal + bedsize
-                if (debug):
-                  print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
-                  print(f'CNA {cna}, cn {cn}, bedsize {bedsize}')
-              if (CNA[cna][iEnd] >= bedStart and CNA[cna][iEnd] <= bedEnd):
-                bedsize = CNA[cna][iEnd] - bedStart
-                cnTotal = cnTotal + cn * bedsize
-                bedsizeTotal = bedsizeTotal + bedsize
-                if (debug):
-                  print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
-                  print(f'CNA {cna}, cn {cn}, bedsize {bedsize}')   
-    # Mark cn as anavailable
-    if (cnTotal==0):
-      w_cn = 'NA'
-      print(f'CNA {cna}, weighted-mean-cn NA')
+    dfChr = df[df[chrColName]==CNA[cna][iChr]]
+    valueTotal = 0
+    bedsizeTotal = 0 #1e-32 # Numerical zero
+    for index, row in dfChr.iterrows():
+      # Assign CNV bed coordinates
+      bedStart = row[startColName]; bedEnd = row[endColName] 
+      # Assign value to obtain weighted-mean for
+      value = row[valueColName]
+      if (debug):
+        # Auxilliary variable
+        chromosome = row[chrColName]
+      if (not pd.isna(value)):
+        # Segment smaller than CNA interval
+        if (bedStart > CNA[cna][iStart] and bedEnd < CNA[cna][iEnd]):
+          bedsize = bedEnd - bedStart
+          valueTotal = valueTotal + value * bedsize
+          bedsizeTotal = bedsizeTotal + bedsize
+          if (debug):
+            print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
+            print(f'CNA {cna}, value {value}, bedsize {bedsize}')
+        # CNA interval smaller than segment
+        if (CNA[cna][iStart] >= bedStart and CNA[cna][iEnd] <= bedEnd):
+          bedsize = CNA[cna][iEnd] - CNA[cna][iStart]
+          valueTotal = valueTotal + value * bedsize
+          bedsizeTotal = bedsizeTotal + bedsize
+          if (debug):
+            print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
+            print(f'CNA {cna}, value {value}, bedsize {bedsize}')
+        # Overlap of CNA interval and segment
+        else:
+          if (CNA[cna][iStart] >= bedStart and CNA[cna][iStart] <= bedEnd):
+            bedsize = bedEnd - CNA[cna][iStart]
+            valueTotal = valueTotal + value * bedsize
+            bedsizeTotal = bedsizeTotal + bedsize
+            if (debug):
+              print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
+              print(f'CNA {cna}, value {value}, bedsize {bedsize}')
+          if (CNA[cna][iEnd] >= bedStart and CNA[cna][iEnd] <= bedEnd):
+            bedsize = CNA[cna][iEnd] - bedStart
+            valueTotal = valueTotal + value * bedsize
+            bedsizeTotal = bedsizeTotal + bedsize
+            if (debug):
+              print(f'CNA {cna}, chromosome {chromosome}, bed-start {bedStart}, bed-end {bedEnd}')
+              print(f'CNA {cna}, value {value}, bedsize {bedsize}')   
+    # Mark value as anavailable
+    if (valueTotal==0):
+      w_value = 'NA'
+      print(f'CNA {cna}, weighted-mean-value NA')
     else:
-      print(f'CNA {cna}, weighted-mean-cn {cnTotal / bedsizeTotal}')
-      w_cn = cnTotal / bedsizeTotal
-    w_cns[cna] = w_cn
-    #print(f'CNA {cna}, weighted-mean-cn {w_cn}')
-  return w_cns
+      print(f'CNA {cna}, weighted-mean-value {valueTotal / bedsizeTotal}')
+      w_value = valueTotal / bedsizeTotal
+    w_values[cna] = w_value
+    #print(f'CNA {cna}, weighted-mean-value {w_value}')
+  return w_values
